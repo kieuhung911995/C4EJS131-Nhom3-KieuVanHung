e5e2b685a0ee43a0d5f8e172966b90cd889cfc53
kieuhung911995 <kieuhung911995@gmail.com>
2022-08-16 14:40:54 +0700

    btvn6

---
+++ bài tập về nhà 6/java.js
@@ -0,0 +1,18 @@
+//BT chung
+//BT1
+let greeting='Hello. My name is John';
+console.log(greeting.charAt(8));
+//BT2
+let str1='The morning is upon us.';
+console.log(str1.slice(12,22));
+//BT3
+let str2='Hello';
+let str3='World';
+console.log(str2.concat(' ',str3));
+
+//BT thầy Tâm
+let str7='Hôm nay ngày thứ 7';
+//BT1
+console.log(str7[0].toLowerCase().concat(str7.slice(1,8),str7[8].toUpperCase(),str7.slice(9,18)));
+//BT2
+console.log(str7[0].toLowerCase().concat(str7.slice(1,3),str7.slice(4,17),'8'));
