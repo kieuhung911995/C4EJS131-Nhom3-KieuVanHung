0dde2f2dfaf55bf1fd212478f3f17a4a546a391b
kieuhung911995 <kieuhung911995@gmail.com>
2022-08-30 10:10:17 +0700

    btvn11

---
+++ bài tập về nhà 11/btvn11.js
@@ -0,0 +1,30 @@
+//BT1 (mẫu)
+/*let click = document.querySelector('.click');
+click.addEventListener('click', () => {
+  color();
+})
+
+function color() {
+  let random_color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = random_color;
+}*/
+
+// BT1 (tự làm chưa chạy đc)
+/*let colors=['ping','red','blue','green','yellow','grey'];
+let i=0;
+document.getElementsByClassName('click')=function changeColor() {
+  if(i>=0 && i<colors.length)
+  {document.body.style.backgroundColor=colors[i]
+  i++}
+}*/
+
+
+//BT2
+/*document.getElementById('btn').style.width='96px';
+document.getElementById('btn').style.height='48px';
+document.getElementById('btn').style.fontSize='24px';
+let i=1;
+document.getElementById('btn').onclick= function increaseNum(){
+    document.getElementById('btn').innerHTML=i;
+    i++
+}*/
