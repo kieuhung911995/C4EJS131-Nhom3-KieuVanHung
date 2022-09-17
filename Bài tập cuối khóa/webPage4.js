b8027897e6b9e427c01235b9a57cea3491e19ddd
kieuhung911995 <110046293+kieuhung911995@users.noreply.github.com>
2022-09-17 19:22:50 +0700

    Add files via upload

--- Bài tập cuối khóa/webPage4.js
+++ Bài tập cuối khóa/webPage4.js
@@ -20,11 +20,6 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   slides[slideIndex - 1].style.display = "block";
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
 };
 //slide text page 2
 let txtPage2 = document.querySelectorAll(".menuTab-page2");
@@ -60,6 +55,123 @@ butt3.addEventListener("click", display3);
 function display3() {
   for (i = 0; i < txtPage2.length; i++) {
     if (i === 2) {
+      txtPage2[i].style.display = "block";
+      another[i].className = "menu-choose";
+    } else {
+      txtPage2[i].style.display = "none";
+      another[i].className = "menu-unChoose";
+    }
+  }
+};
+let butt4 = document.querySelector("#butt4");
+butt4.addEventListener("click", display4);
+function display4() {
+  for (i = 0; i < txtPage2.length; i++) {
+    if (i === 3) {
+      txtPage2[i].style.display = "block";
+      another[i].className = "menu-choose";
+    } else {
+      txtPage2[i].style.display = "none";
+      another[i].className = "menu-unChoose";
+    }
+  }
+};
+let butt5 = document.querySelector("#butt5");
+butt5.addEventListener("click", display5);
+function display5() {
+  for (i = 0; i < txtPage2.length; i++) {
+    if (i === 4) {
+      txtPage2[i].style.display = "block";
+      another[i].className = "menu-choose";
+    } else {
+      txtPage2[i].style.display = "none";
+      another[i].className = "menu-unChoose";
+    }
+  }
+};
+let butt6 = document.querySelector("#butt6");
+butt6.addEventListener("click", display6);
+function display6() {
+  for (i = 0; i < txtPage2.length; i++) {
+    if (i === 5) {
+      txtPage2[i].style.display = "block";
+      another[i].className = "menu-choose";
+    } else {
+      txtPage2[i].style.display = "none";
+      another[i].className = "menu-unChoose";
+    }
+  }
+};
+let butt7 = document.querySelector("#butt7");
+butt7.addEventListener("click", display7);
+function display7() {
+  for (i = 0; i < txtPage2.length; i++) {
+    if (i ===6) {
+      txtPage2[i].style.display = "block";
+      another[i].className = "menu-choose";
+    } else {
+      txtPage2[i].style.display = "none";
+      another[i].className = "menu-unChoose";
+    }
+  }
+};
+let butt8 = document.querySelector("#butt8");
+butt8.addEventListener("click", display8);
+function display8() {
+  for (i = 0; i < txtPage2.length; i++) {
+    if (i === 7) {
+      txtPage2[i].style.display = "block";
+      another[i].className = "menu-choose";
+    } else {
+      txtPage2[i].style.display = "none";
+      another[i].className = "menu-unChoose";
+    }
+  }
+};
+let butt9 = document.querySelector("#butt9");
+butt9.addEventListener("click", display9);
+function display9() {
+  for (i = 0; i < txtPage2.length; i++) {
+    if (i === 8) {
+      txtPage2[i].style.display = "block";
+      another[i].className = "menu-choose";
+    } else {
+      txtPage2[i].style.display = "none";
+      another[i].className = "menu-unChoose";
+    }
+  }
+};
+let butt10 = document.querySelector("#butt10");
+butt10.addEventListener("click", display10);
+function display10() {
+  for (i = 0; i < txtPage2.length; i++) {
+    if (i === 9) {
+      txtPage2[i].style.display = "block";
+      another[i].className = "menu-choose";
+    } else {
+      txtPage2[i].style.display = "none";
+      another[i].className = "menu-unChoose";
+    }
+  }
+};
+let butt11 = document.querySelector("#butt11");
+butt11.addEventListener("click", display11);
+function display11() {
+  for (i = 0; i < txtPage2.length; i++) {
+    if (i === 10) {
+      txtPage2[i].style.display = "block";
+      another[i].className = "menu-choose";
+    } else {
+      txtPage2[i].style.display = "none";
+      another[i].className = "menu-unChoose";
+    }
+  }
+};
+let butt12 = document.querySelector("#butt12");
+butt12.addEventListener("click", display12);
+function display12() {
+  for (i = 0; i < txtPage2.length; i++) {
+    if (i === 11) {
       txtPage2[i].style.display = "block";
       another[i].className = "menu-choose";
     } else {
